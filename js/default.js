d5c03991acd8c2b38394ea607103278532dd9af5
Elke Heymans <elke@Elkes-MacBook-Pro-2.local>
2017-09-07 10:12:29 +0200

    Added smooth scrolling

--- js/default.js
+++ js/default.js
@@ -1,7 +1,19 @@
+// reduced header when scrolling
 $(window).scroll(function() {
 	if ($(document).scrollTop() > 120) {
 		$('body > header').addClass('scrolled');
 	} else {
 		$('body > header').removeClass('scrolled');
 	}
+});
+
+// smooth scrolling for # links
+$(document).on('click', 'a[href^="#"]', function(e) {
+    var element = $($(this).attr('href'));
+    if (element.length === 0) {
+        return;
+    }
+    e.preventDefault();
+    var pos = element.offset().top;
+    $('body, html').animate({scrollTop: element.offset().top});
 });
