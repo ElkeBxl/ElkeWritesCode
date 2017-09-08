6ac8edead9a11993beebf9b75b17a4fb86a602f9
Elke Heymans <elke@Elkes-MacBook-Pro-2.local>
2017-09-08 10:48:59 +0200

    Added animation to progress bars

--- js/default.js
+++ js/default.js
@@ -1,10 +1,20 @@
-// reduced header when scrolling
 $(window).scroll(function() {
+    // reduced header when scrolling
 	if ($(document).scrollTop() > 120) {
 		$('body > header').addClass('scrolled');
 	} else {
 		$('body > header').removeClass('scrolled');
 	}
+
+    // activation of progress bars animation
+    $(".progress-bar").each(function(index, el) {
+        el = $(el);
+        if (el.offset().top <= $(document).scrollTop() + window.innerHeight) {
+            el.css("width", el.attr("aria-valuenow") + "%");
+        } else {
+            el.css("width", 0);
+        }
+    });
 });
 
 // smooth scrolling for # links
@@ -16,4 +26,9 @@ $(document).on('click', 'a[href^="#"]', function(e) {
     e.preventDefault();
     var pos = element.offset().top;
     $('body, html').animate({scrollTop: element.offset().top});
+});
+
+// animated progress bars
+$(document).ready(function() {
+    $(".progress-bar").css("width", 0);
 });
